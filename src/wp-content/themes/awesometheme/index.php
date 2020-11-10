1e630096a0a3b48cf4745139ffa8e049a8194f39
M-Lo317 <mlo.galloway@gmail.com>
2020-11-10 14:50:04 +0200

    Layout Changes, Font Added and created Sidebars/Widgets

--- src/wp-content/themes/awesometheme/index.php
+++ src/wp-content/themes/awesometheme/index.php
@@ -1,17 +1,29 @@
 <?php get_header(); ?>
 
-    <?php 
-    
-    if(have_posts() ):
+<div class="row">
 
-        while( have_posts() ): the_post(); ?>
-            
-            <?php get_template_part('content', get_post_format()); ?>
+    <div class="col-xs-12 col-sm-8">
 
-        <?php endwhile;
+        <?php 
+        
+        if(have_posts() ):
 
-    endif;
+            while( have_posts() ): the_post(); ?>
+                
+                <?php get_template_part('content', get_post_format()); ?>
 
-    ?>
+            <?php endwhile;
+
+        endif;
+
+        ?>
+
+    </div>
+
+    <div class="col-xs-12 col-sm-4">
+        <?php get_sidebar(); ?>
+    </div>
+
+</div>
 
 <?php get_footer(); ?>
